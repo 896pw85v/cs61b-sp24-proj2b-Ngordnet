0a841b11674d01537e0208b591354bbfe41ca53b
hmmmm <jinglin.he@edu.sait.ca>
2026-03-10 21:51:25 -0600

    Implemented loading from CSV, tested loading, tested getting index on large data set

--- tests/TestGraph.java
+++ tests/TestGraph.java
@@ -1,6 +1,7 @@
 import graph.Synset;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
 import java.util.TreeSet;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -41,5 +42,43 @@ public class TestGraph {
         assertThat(synset.getIndices("you")).containsExactly(1, 2);
 
         assertThat(synset.getIndices("unrelated")).isEmpty();
+    }
+
+    @Test
+    public void testIO() {
+        Synset ioset = new Synset("data/data/wordnet/synsets11.txt");
+        assertThat(ioset).isNotNull();
+        assertThat(ioset.get(0)).containsExactly("action");
+        assertThat(ioset.get(4)).containsExactly("jump", "parachuting");
+        assertThat(ioset.get(10)).containsExactly("actifed");
+        assertThat(ioset.get(-1)).isEmpty();
+
+        try {
+            Synset wrongset = new Synset("wrong path");
+            assertThat(wrongset).isNotNull();
+            assertThat(wrongset.get(0)).isEmpty();
+            assertThat(wrongset.table).isEmpty();
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testLargeFile() {
+        Synset largeset = new Synset("data/data/wordnet/synsets.txt");
+        System.out.println(largeset.table);
+        System.out.println(largeset.table.size());
+    }
+
+    @Test
+    public void testIndicesLarge() {
+        Synset largeset = new Synset("data/data/wordnet/synsets.txt");
+        assertThat(largeset.getIndices("1530s")).contains(1);
+        assertThat(largeset.getIndices("1830s")).contains(10);
+        assertThat(largeset.getIndices("derivative")).contains(34695);
+        assertThat(largeset.getIndices("propulsion")).contains(63362);
+        assertThat(largeset.getIndices("zymosis")).contains(82191);
+        // i don't really know what these words belong to so only testing one index
+        // finished in around 720ms for 82191 entries, pretty fast
     }
 }
